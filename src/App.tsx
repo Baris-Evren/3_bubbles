da0405fbc8769685af86b4a6950bfdfaf6505b7c
Baris-Evren <146993887+Baris-Evren@users.noreply.github.com>
2024-12-15 01:06:09 +0300

    Update App.tsx
    Original: BrowserRouter
    Edited: HashRouter  (https://answers.netlify.com/t/support-guide-i-ve-deployed-my-site-but-i-still-see-page-not-found/125/20?utm_source=404page&utm_campaign=community_tracking)
    
    Used again with original. Because just used "#" for "/admin/login" page access like "#/admin/login".

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import Cart from './components/Cart';
